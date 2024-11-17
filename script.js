1e872913daa60deff582a31d69d72b5aad23bf3c
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-11-17 13:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+
+const  promise = (age, name) => { 
+	return new Promise( (resolve, reject) => {
+	if(age>18){
+		resolve(`Welcome, ${name}. You can vote.`)
+	}else{
+		resolve(`Oh sorry ${name}. You aren't old enough.`)
+	}
+} )}
+
+
+
+function form_submit(event){
+	event.preventDefault()
+	let age = document.getElementById("age").value;
+	let name = document.getElementById("name").value;
+
+	if(age==0 || name.length==0){
+		alert("Check Credentials")
+
+	}
+
+	promise(age,name).then( (data)=>{
+		console.log(data)
+
+	} )
+
+	
+
